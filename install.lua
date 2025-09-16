15896a12e5dc9cf38e2c162737e637cd36e347e7
TuShaggy <fabiandelcazo2001@gmail.com>
2025-09-16 23:09:01 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -13,7 +13,20 @@ local files = {
 local function downloadFile(path)
   local url = baseURL .. path
   print("Downloading " .. path .. " from " .. url)
-  shell.run("wget", url, path)
+  local file = http.get(url)
+  if file then
+    local content = file.readAll()
+    local f = fs.open(path, "w")
+    if f then
+      f.write(content)
+      f.close()
+    else
+      print("Error: Could not open " .. path .. " for writing")
+    end
+    file.close()
+  else
+    print("Error: Could not download " .. path)
+  end
 end
 
 for _, path in ipairs(files) do
