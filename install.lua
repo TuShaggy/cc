645fea19f30f64ce2d2b8f08af246ecedec24372
TuShaggy <fabiandelcazo2001@gmail.com>
2025-09-17 02:12:30 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,6 +1,6 @@
 -- drmon installation script
 --
-local version = "1.0.7"
+local version = "1.1.0"
 local repoBaseUrl = "https://raw.githubusercontent.com/TuShaggy/cc/main/"
 
 local filesToDownload = {
